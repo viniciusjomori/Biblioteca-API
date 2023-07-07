9cc8a637a4fb53f5157406a0734956264d5fbb90
viniciusjomori <yuitivinicius@gmail.com>
2023-07-07 12:31:50 -0300

    adicionando bean de cryptografia

--- src/main/java/com/br/Library/security/SecurityConfig.java
+++ src/main/java/com/br/Library/security/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -68,6 +70,11 @@ public class SecurityConfig {
                 """; 
         roleHierarchy.setHierarchy(hierarchy);
         return roleHierarchy;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
