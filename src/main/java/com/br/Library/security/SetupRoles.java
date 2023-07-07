6957368998e9d0829b62d42a30c93c03f72a04dc
viniciusjomori <yuitivinicius@gmail.com>
2023-07-07 16:52:06 -0300

    adicionado usuarios de teste

--- src/main/java/com/br/Library/security/SetupRoles.java
+++ src/main/java/com/br/Library/security/SetupRoles.java
@@ -1,17 +1,18 @@
 package com.br.Library.security;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import com.br.Library.dto.EmployeeRequestDTO;
-import com.br.Library.dto.UserRequestDTO;
 import com.br.Library.enums.RoleName;
 import com.br.Library.model.RoleModel;
+import com.br.Library.model.UserModel;
 import com.br.Library.repository.RoleRepository;
-import com.br.Library.service.ClientService;
-import com.br.Library.service.EmployeeService;
+import com.br.Library.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -24,37 +25,54 @@ public class SetupRoles implements ApplicationListener<ContextRefreshedEvent> {
     private RoleRepository roleRepository;
 
     @Autowired
-    private ClientService clientService;
+    private UserRepository userRepository;
 
     @Autowired
-    private EmployeeService employeeService;
+    private PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         if(alreadySetup) return;
         
-        createRoleIfNotFound(RoleName.ROLE_CLIENT);
+        RoleModel clientRole = createRoleIfNotFound(RoleName.ROLE_CLIENT);
 
-        createRoleIfNotFound(RoleName.ROLE_EMPLOYEE);
+        RoleModel employeeRole = createRoleIfNotFound(RoleName.ROLE_EMPLOYEE);
 
-        createRoleIfNotFound(RoleName.ROLE_ADMINISTRATOR);
+        RoleModel admRole = createRoleIfNotFound(RoleName.ROLE_ADMINISTRATOR);
 
-        /* clientService.createClient(new UserRequestDTO("client", "senha"));
-        employeeService.createEmployee(new EmployeeRequestDTO("employee", "senha", "ROLE_EMPLOYEE"));
-        employeeService.createEmployee(new EmployeeRequestDTO("adm", "senha", "ROLE_ADMINISTRATOR")); */
+        createUserIfNotFound(1, "client", "senha", clientRole);
+        createUserIfNotFound(2, "employee", "senha", employeeRole);
+        createUserIfNotFound(3, "adm", "senha", admRole);
 
+        
         alreadySetup = true;
     }
 
     @Transactional
-    public void createRoleIfNotFound(RoleName roleName) {
-        if(roleRepository.existsByName(roleName)) return;
+    public RoleModel createRoleIfNotFound(RoleName roleName) {
+        
+        Optional<RoleModel> optional = roleRepository.findByName(roleName);
 
-        RoleModel roleModel = new RoleModel();
-        roleModel.setName(roleName);
+        if(optional.isPresent()) {
+            return optional.get();
+        }
 
-        roleRepository.save(roleModel);
+        RoleModel role = new RoleModel();
+        role.setName(roleName);
+        return roleRepository.save(role);
+    }
+
+    @Transactional
+    public void createUserIfNotFound(long id, String username, String password, RoleModel role) {
+        if(!userRepository.existsById(id)) {
+            UserModel user = new UserModel();
+            user.setId(id);
+            user.setUsername(username);
+            user.setPassword(passwordEncoder.encode(password));
+            user.setRole(role);
+            userRepository.save(user);
+        }
     }
 
 }
