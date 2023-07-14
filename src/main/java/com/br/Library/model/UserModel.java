22d9fd8bce96c2ed0e518321a35bfc76539b2332
viniciusjomori <yuitivinicius@gmail.com>
2023-07-14 11:26:19 -0300

    corrigido: fetchtype removido

--- src/main/java/com/br/Library/model/UserModel.java
+++ src/main/java/com/br/Library/model/UserModel.java
@@ -2,7 +2,6 @@ package com.br.Library.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,7 +39,7 @@ public class UserModel implements UserDetails {
     @Column
     private boolean enabled;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne
     @JoinColumn(name = "role_id")
     private RoleModel role;
 
