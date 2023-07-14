af27a27d42c488f8c9273d5e63379a0b4a54b760
viniciusjomori <yuitivinicius@gmail.com>
2023-07-14 11:27:03 -0300

    corrigido: add private

--- src/main/java/com/br/Library/model/BookModel.java
+++ src/main/java/com/br/Library/model/BookModel.java
@@ -33,8 +33,8 @@ public class BookModel {
     @Column
     private int availableCopies;
     @OneToMany(mappedBy = "book")
-    Collection<LoanModel> loans;
+    private Collection<LoanModel> loans;
     @OneToMany(mappedBy = "book")
-    Collection<ReserveModel> reserves;
+    private Collection<ReserveModel> reserves;
 
 }
